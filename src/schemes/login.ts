4e33c47856af8fa6520191a1037a8b8854b3a366
Tobias Feld <tobias.feld@hotmail.com>
2021-02-27 15:45:59 +0100

    Fix incorrect capitalization for imported package joi

--- src/schemes/login.ts
+++ src/schemes/login.ts
@@ -1,4 +1,4 @@
-import Joi from "Joi"
+import Joi from "joi"
 export default Joi.object({
     username: Joi.string().alphanum().min(3).max(30).required(),
     password: Joi.string().min(8).max(24).required(),
--- src/schemes/setupNew.ts
+++ src/schemes/setupNew.ts
@@ -1,4 +1,4 @@
-import Joi from "Joi"
+import Joi from "joi"
 
 let validBaudrates = [
     "Auto",
