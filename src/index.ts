92c0b3a4267f293eef1b467a247dc1288940dd8a
Tobias Feld <tobias.feld@hotmail.com>
2021-03-27 15:01:35 +0100

    Add catch clause for auto-connecting devices

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,13 @@ const currentState = new stateManager()
 // // Find initial printers on boot.
 currentState.storage.listDevices().then((devices: Device[]) => {
     if (devices.length > 0) {
-        currentState.connectionManager.create(
-            devices[0].path,
-            isNaN(parseInt(devices[0].baud)) ? null : parseInt(devices[0].baud)
-        )
+        currentState.connectionManager
+            .create(
+                devices[0].path,
+                isNaN(parseInt(devices[0].baud))
+                    ? null
+                    : parseInt(devices[0].baud)
+            )
+            .catch(console.error)
     }
 })
